e363fefdb91feb7c5913e069c26fd45228e59380
Pedroexe23 <pedro.p.alfaro.m@gmail.com>
2021-04-08 11:33:12 -0400

    agregar servidor

---
+++ ChatserverApp/ChatserverApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using ChatserverApp.comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatserverApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = Int32.Parse(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando Servidor en puerto {0}", puerto);
+            ServerSocket servidor = new ServerSocket(puerto);
+            if (servidor.iniciar())
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Esperando Clientes");
+                if (servidor.ObtenerCliente())
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Conexion Establecida");
+                    Console.WriteLine("S: Hola Mundo Cliente");
+                    servidor.Escribir("Hola Mundo Cliente");
+                    String mensaje = servidor.Leer();
+                    Console.WriteLine("C:{0}", mensaje);
+                    
+                    servidor.CerraConexion();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("no es posible iniciar Servidor");
+                Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
---
+++ ChatserverApp/ChatserverApp/comunicacion/ServerSocket.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatserverApp.comunicacion
+{
+    class ServerSocket
+    {
+        private int puerto;
+        private Socket servidor;
+        private Socket comCliente;
+
+        private StreamWriter writer;
+        private StreamReader reader;
+
+        public ServerSocket(int puerto)
+        {
+            this.puerto = puerto;
+        }
+
+        public bool iniciar()
+        {
+            try
+            {
+                this.servidor = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                this.servidor.Bind(new IPEndPoint(IPAddress.Any, this.puerto));
+                this.servidor.Listen(10);
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                return false;
+            }
+
+
+        }
+
+        public bool ObtenerCliente()
+        {
+            try
+            {
+                this.comCliente = this.servidor.Accept();
+                Stream stream = new NetworkStream(this.comCliente);
+                this.writer = new StreamWriter(stream);
+                this.reader = new StreamReader(stream);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool Escribir(string Mensaje)
+        {
+            try
+            {
+                this.writer.WriteLine(Mensaje);
+                this.writer.Flush();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+        }
+        public string Leer()
+        {
+            try
+            {
+                return this.reader.ReadLine().Trim();
+            }
+            catch (Exception ex)
+            {
+
+                return null;
+            }
+        }
+        public void CerraConexion()
+        {
+            this.comCliente.Close();
+        }        
+    }
+}
